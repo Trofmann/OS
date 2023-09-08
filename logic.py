67454fa6ca7672a0ec425f1f6961a0d59d0a2b82
Evgeniy <trofimovevgeniy9@gmail.com>
2023-09-08 21:39:23 +0300

    Проверять надо тут

--- logic.py
+++ logic.py
@@ -5,5 +5,6 @@ from scheduler import scheduler
 def load_new_task():
     """Загрузка новой задачи"""
     # Загрузка новой задачи есть создание процесса с этой задачей и добавление процесс в планировщик
+    # TODO: Проверять свободную память
     process_ = ProcessFabric.generate_random()
     scheduler.append_process(process_)
