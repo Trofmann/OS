5f6ac524041a70d99f626167b389a9d3a861683c
Evgeniy <trofimovevgeniy9@gmail.com>
2023-09-10 17:22:39 +0300

    Лишнее

--- logic.py
+++ logic.py
@@ -6,7 +6,6 @@ from system import system
 def load_new_task() -> bool:
     """Загрузка новой задачи"""
     # Загрузка новой задачи есть создание процесса с этой задачей и добавление процесс в планировщик
-    # TODO: Проверять свободную память
     process_ = ProcessFabric.generate_random()
     if system.get_empty_memory() >= process_.get_used_memory():
         scheduler.append_process(process_)
