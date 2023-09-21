55d3dfdd7403eb1ca02a0d97e1f020641343ea98
Evgeniy <trofimovevgeniy9@gmail.com>
2023-09-21 18:31:35 +0300

    СТроковое представление

--- statistics.py
+++ statistics.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import List
+
 from system import system
 
 
@@ -20,32 +21,27 @@ class SystemStatisticsProxy(object):
         # self.system = system
         self._process_data = list()  # type:List[ProcessStartStopData]
 
-    @property
-    def loaded_tasks_count(self) -> int:
+    def _get_loaded_tasks_count(self) -> int:
         """Число загруженных задач"""
         from scheduler import scheduler
         return len(scheduler.get_processes())
 
-    @property
-    def system_costs(self) -> str:
+    def _get_system_costs(self) -> str:
         """Системные затраты в процентах"""
         value = 0
         if system.tacks_count:
             value = round((system.system_tacks_count / system.tacks_count) * 100, 0)
         return f'{value} %'
 
-    @property
-    def working_time(self) -> int:
+    def _get_working_time(self) -> int:
         """Время работы системы в тактах"""
         return system.tacts_count
 
-    @property
-    def completed_tasks_count(self) -> int:
+    def _get_completed_tasks_count(self) -> int:
         """Число выполненных задач"""
         return system.completed_tasks_count
 
-    @property
-    def turnaround_time(self) -> float:
+    def _get_turnaround_time(self) -> float:
         """Оборотное время"""
         # Находится как среднестатистическое время, прошедшее с момента загрузки процесса в систему,
         # до его вывода из системы в связи с завершением
@@ -54,6 +50,21 @@ class SystemStatisticsProxy(object):
 
     def add_process_data(self, start: int, stop: int) -> None:
         self._process_data.append(ProcessStartStopData(start, stop))
+
+    def dump(self) -> str:
+        info_funcs = [
+            self._get_loaded_tasks_count,
+            self._get_completed_tasks_count,
+            self._get_working_time,
+            self._get_system_costs,
+            self._get_turnaround_time,
+        ]
+        info = []
+        for func in info_funcs:
+            name = func.__doc__
+            value = func.__call__()
+            info.append(f'{name}: {value}')
+        return '\n'.join(info)
 
 
 statistics = SystemStatisticsProxy()
